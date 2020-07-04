95574b684f81ff96c7207f73c2bdd91440324efa
J.P.Ragey <jpragey@orange.fr>
2020-07-04 18:46:23 +0200

    Oops, forgot 2 files

---
+++ org.sirius.frontend/src/main/java/org/sirius/frontend/core/parser/MemberValueDeclarationParser.java
@@ -0,0 +1,42 @@
+package org.sirius.frontend.core.parser;
+
+import java.util.Optional;
+
+import org.sirius.common.error.Reporter;
+import org.sirius.frontend.ast.AnnotationList;
+import org.sirius.frontend.ast.AstExpression;
+import org.sirius.frontend.ast.AstInterfaceDeclaration;
+import org.sirius.frontend.ast.AstMemberValueDeclaration;
+import org.sirius.frontend.ast.AstToken;
+import org.sirius.frontend.ast.AstType;
+import org.sirius.frontend.parser.SiriusBaseVisitor;
+import org.sirius.frontend.parser.SiriusParser.MemberValueDeclarationContext;
+
+public class MemberValueDeclarationParser {
+
+	public static class MemberValueVisitor extends SiriusBaseVisitor<AstMemberValueDeclaration> {
+		private Reporter reporter;
+
+		public MemberValueVisitor(Reporter reporter) {
+			super();
+			this.reporter = reporter;
+		}
+
+		@Override
+		public AstMemberValueDeclaration visitMemberValueDeclaration(MemberValueDeclarationContext ctx) {
+			// TODO: annotationList
+			AnnotationList annotations = new AnnotationList(); // TODO
+			
+			TypeParser.TypeVisitor typeVisitor = new TypeParser.TypeVisitor(reporter);
+			AstType type = ctx.type.accept(typeVisitor);
+			AstToken name = new AstToken(ctx.LOWER_ID);
+			
+			ExpressionParser.ExpressionVisitor expressionVisitor = new ExpressionParser.ExpressionVisitor(reporter);
+			Optional<AstExpression> initialValue = (ctx.expression == null) ?  Optional.empty() : Optional.of(ctx.expression.accept(expressionVisitor));
+
+			return new AstMemberValueDeclaration(annotations, type, name, initialValue);
+		}
+		
+		
+	}
+}
---
+++ org.sirius.frontend/src/test/java/org/sirius/frontend/core/parser/MemberValueDeclarationParserTest.java
@@ -0,0 +1,73 @@
+package org.sirius.frontend.core.parser;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.sirius.common.core.QName;
+import org.sirius.common.error.AccumulatingReporter;
+import org.sirius.common.error.Reporter;
+import org.sirius.common.error.ShellReporter;
+import org.sirius.frontend.api.Expression;
+import org.sirius.frontend.ast.AstExpression;
+import org.sirius.frontend.ast.AstIntegerConstantExpression;
+import org.sirius.frontend.ast.AstInterfaceDeclaration;
+import org.sirius.frontend.ast.AstMemberValueDeclaration;
+import org.sirius.frontend.ast.AstType;
+import org.sirius.frontend.ast.SimpleReferenceExpression;
+import org.sirius.frontend.ast.SimpleType;
+import org.sirius.frontend.ast.TypeParameter;
+import org.sirius.frontend.ast.Variance;
+import org.sirius.frontend.core.parser.TypeParameterParser.TypeParameterVisitor;
+import org.sirius.frontend.parser.SiriusParser;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class MemberValueDeclarationParserTest {
+
+	private Reporter reporter ;
+	
+	@BeforeEach
+	public void setup() {
+		this.reporter = new AccumulatingReporter(new ShellReporter());
+	}
+	@AfterEach
+	public void tearDown() {
+		assert(this.reporter.ok());
+	}
+
+	@Test
+	@DisplayName("Simple member values")
+	public void simpleMemberValue() {
+		parseMemberValue("Integer i;",      ((AstMemberValueDeclaration memberValue) -> {
+			assertThat(memberValue.getNameString(), equalTo("i") );
+			assertThat(memberValue.getApiInitialValue().isPresent(), equalTo(false) );
+			assertThat(memberValue.getType(), instanceOf(SimpleType.class));
+			assertThat(((SimpleType)memberValue.getType()).getNameString(), equalTo("Integer"));
+			}) );
+		parseMemberValue("Integer i = 42;",      ((AstMemberValueDeclaration memberValue) -> {
+			assertThat(memberValue.getApiInitialValue().isPresent(), equalTo(true) );
+			AstExpression initExpr = memberValue.getInitialValue().get();
+			assertThat(initExpr, instanceOf(AstIntegerConstantExpression.class));
+			assertThat(((AstIntegerConstantExpression)initExpr).getValue(), equalTo(42));
+			}) );
+	}
+
+	private AstMemberValueDeclaration parseMemberValue(String inputText, Consumer<AstMemberValueDeclaration> verify) {
+		SiriusParser parser = ParserUtil.createParser(reporter, inputText);
+		ParseTree tree = parser.memberValueDeclaration();
+		
+		MemberValueDeclarationParser.MemberValueVisitor visitor = new MemberValueDeclarationParser.MemberValueVisitor(reporter);
+		AstMemberValueDeclaration memberValue = visitor.visit(tree);
+		
+		verify.accept(memberValue);
+		return memberValue;
+	}
+}
