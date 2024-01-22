ab0a84337816d6864960d0423c46ece13469ff62
Tarandeep Singh <tarandeep.singh562@gmail.com>
2024-01-21 16:17:07 -0800

    jdk-inconsistencies

--- org.sirius.frontend/src/main/java/module-info.java
+++ org.sirius.frontend/src/main/java/module-info.java
@@ -4,7 +4,6 @@
  *
  */
 module org.sirius.frontend {
-	requires transitive org.sirius.common;
 //	requires org.junit.jupiter.api;
 	
 	requires org.antlr.antlr4.runtime;
@@ -13,7 +12,6 @@ module org.sirius.frontend {
 //	exports org.sirius.frontend;
 	exports org.sirius.frontend.api;
 //	exports org.sirius.frontend.apiimpl to org.sirius.backend.jvm;	// TODO: remove (used for test only ? move frontend api to its own module ?)
-	exports org.sirius.frontend.apiimpl;	// TODO: remove (used for test only ? move frontend api to its own module ?)
 	
 //	opens org.sirius.frontend.apiimpl;
 	
